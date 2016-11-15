0701961df8110af7ab6dfc4a3cfcc8a1cc5ecf2d
root <fpxg37@mail.ru>
2016-11-15 14:30:00 +0500

    Third attempt

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
-#! /bin/bash
+#!/bin/sh
 
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
+
+sudo cp -f etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+
+#sudo cp -f /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start﻿
 
